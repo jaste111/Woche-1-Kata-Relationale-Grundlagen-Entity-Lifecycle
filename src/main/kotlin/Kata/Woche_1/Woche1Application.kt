dd01230acfd05df8bd7914a098c54c477e9eeabc
Jan Steimann <jan.steimann@zollsoft.de>
2026-01-09 10:06:45 +0100

    Add spring

---
+++ src/main/kotlin/Kata/Woche_1/Woche1Application.kt
@@ -0,0 +1,11 @@
+package Kata.Woche_1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Woche1Application
+
+fun main(args: Array<String>) {
+	runApplication<Woche1Application>(*args)
+}
---
+++ src/test/kotlin/Kata/Woche_1/Woche1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package Kata.Woche_1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Woche1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
